8e89db0257a62177c56a9ed3cb052739d3388e9e
robonamari <ROBONAMARI@GMAIL.COM>
2025-03-11 02:45:58 +0330

    dev

--- SECURITY.md
+++ SECURITY.md
@@ -4,18 +4,16 @@
 
 | Version | Supported          |
 | ------- | ------------------ |
-| 1.2.0   | :white_check_mark: |
-| 1.1.0   | :white_check_mark: |
-| 1.0.0   | :x:                |
+| 1.0.0   | :white_check_mark: |
 
 ## Reporting a Vulnerability
 
 If you discover a vulnerability in this project, please report it as soon as possible to help us address it quickly and responsibly.
 
-You can report security vulnerabilities by opening an issue at our GitHub repository:
+You can report security vulnerabilities by opening an issue at our GitHub repository:  
 [GitHub Issue Tracker](https://github.com/robonamari/Dirlotix-py/issues)
 
-For critical or more urgent vulnerabilities, you may email us directly at:
+For critical or more urgent vulnerabilities, you may email us directly at:  
 `robonamari@gmail.com`
 
 We will respond to your report as **soon as possible**. After we assess the severity, we will either accept or decline the report. If accepted, we will work on a fix and release it as soon as possible.
--- index.py
+++ index.py
@@ -38,7 +38,7 @@ def redirect_to_default_lang():
     return redirect(new_url, code=302)
 
 
-@app.route("/<lang>", methods=["GET"])
+@app.route("/<lang>/", methods=["GET"])
 def index(lang):
     valid_languages = {
         filename[:-5]
@@ -60,7 +60,7 @@ def index(lang):
                 "icon": "fas fa-level-up-alt",
                 "name": translations["Parent_Directory"],
                 "link": (
-                    f"/{lang}?dir={os.path.dirname(directory)}"
+                    f"/{lang}/?dir={os.path.dirname(directory)}"
                     if os.path.dirname(directory)
                     else f"/{lang}"
                 ),
@@ -70,7 +70,7 @@ def index(lang):
         else []
     )
     for file in sorted(
-        {file for file in os.listdir(directory) if not file.startswith(".")}
+        [file for file in os.listdir(directory) if not file.startswith(".")]
     ):
         if file in os.getenv("ignore_files", "").split(","):
             continue
@@ -125,174 +125,59 @@ def index(lang):
                 {
                     "icon": "fas fa-folder-open",
                     "name": file,
-                    "link": f"/{lang}?dir={os.path.relpath(file_path, os.path.dirname(__file__))}",
+                    "link": f"/{lang}/?dir={os.path.relpath(file_path, os.path.dirname(__file__))}",
                 }
             )
     return render_template_string(
-        """
-<!doctype html>
-<html dir="{{ translations['head']['dir'] }}" lang="{{ lang }}">
+        """<!doctypehtml>
+         <html dir="{{ translations["head"]["dir"] }}" lang="{{ lang }}">
 
-<head>
-    <meta charset="UTF-8">
-    <title>{{ translations['directory_listing'] }}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="{{ theme_color }}">
-    <meta name="title" content="{{ translations['head']['title'] }} - {{ ' '.join(request.host.split(':')[0].split('.')[:-1]) }}">
-    <meta name="description" content="{{ translations['head']['description'] }}">
-    <meta property="og:title" content="{{ translations['head']['title'] }} - {{ ' '.join(request.host.split(':')[0].split('.')[:-1]) }}">
-    <meta property="og:description" content="{{ translations['head']['description'] }}">
-    <meta property="og:type" content="article">
-    <meta property="og:url" content="{{ request.url_root }}">
-    <meta property="og:image" content="{{ favicon }}">
-    <meta property="og:site_name" content="{{ ' '.join(request.host.split(':')[0].split('.')[:-1]) }}">
-    <meta property="og:locale" content="{{ lang }}">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        body {
-        font-family: {{ font_family }};
-        margin: 20px;
-        background-color: #f9f9f9;
-        color: #333;
-        }
-        h1 {
-        text-align: center;
-        color: #555;
-        }
-        .table-container {
-        max-width: 100%;
-        margin: 0 auto;
-        overflow-x: auto;
-        }
-        table {
-        border-collapse: collapse;
-        width: 100%;
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, .1);
-        margin-bottom: 20px;
-        }
-        td, th {
-        padding: 12px;
-        text-align: left;
-        border-bottom: 1px solid #ddd;
-        }
-        th {
-        background-color: #f2f2f2;
-        cursor: pointer;
-        }
-        th.sortable:hover {
-        background-color: #e0e0e0;
-        }
-        .icon {
-        text-align: center;
-        width: 30px;
-        }
-        a {
-        text-decoration: none;
-        color: #007bff;
-        font-weight: 700;
-        }
-        a:hover {
-        text-decoration: underline;
-        }
-        .search-bar {
-        margin-bottom: 20px;
-        text-align: center;
-        }
-        .search-bar input[type="text"] {
-        width: 300px;
-        max-width: 80%;
-        padding: 10px;
-        border: 1px solid #ddd;
-        border-radius: 4px;
-        }
-        .search-bar button {
-        padding: 10px 20px;
-        border: none;
-        background-color: #007bff;
-        color: #fff;
-        border-radius: 4px;
-        cursor: pointer;
-        }
-        .search-bar button:hover {
-        background-color: #0056b3;
-        }
-        @media (max-width: 576px) {
-        .search-bar input[type="text"] {
-        width: 80%;
-        }
-        }
-    </style>
-</head>
+         <head>
+          <meta charset="UTF-8">
+          <title>{{ translations["directory_listing"] }}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="theme-color" content="{{ theme_color }}">
+          <meta name="title" content="{{ translations["head"]["title"] }} - {{ " ".join(request.host.split(":")[0].split(".")[:-1]) }}">
+          <meta name="description" content="{{ translations["head"]["description"] }}">
+          <meta property="og:title" content="{{ translations["head"]["title"] }} - {{ " ".join(request.host.split(":")[0].split(".")[:-1]) }}">
+          <meta property="og:description" content="{{ translations["head"]["description"] }}">
+          <meta property="og:type" content="article">
+          <meta property="og:url" content="{{ request.url_root }}">
+          <meta property="og:image" content="{{ favicon }}">
+          <meta property="og:site_name" content="{{ " ".join(request.host.split(":")[0].split(".")[:-1]) }}">
+          <meta property="og:locale" content="{{ lang }}">
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
+          <style>
+           body{font-family:{{ font_family }};margin:20px;background-color:#f9f9f9;color:#333}h1{text-align:center;color:#555}table{border-collapse:collapse;width:100%;background-color:#fff;box-shadow:0 0 10px rgba(0,0,0,.1);margin-bottom:20px}td,th{padding:12px;text-align:left;border-bottom:1px solid #ddd}th{background-color:#f2f2f2;cursor:pointer}th.sortable:hover{background-color:#e0e0e0}.icon{text-align:center;width:30px}a{text-decoration:none;color:#007bff;font-weight:700}a:hover{text-decoration:underline}.search-bar{margin-bottom:20px;text-align:center}.search-bar input[type=text]{width:300px;max-width:80%;padding:10px;border:1px solid #ddd;border-radius:4px}.search-bar button{padding:10px 20px;border:none;background-color:#007bff;color:#fff;border-radius:4px;cursor:pointer}.search-bar button:hover{background-color:#0056b3}
+          </style>
+         </head>
 
-<body>
-    <h1>{{ translations['directory_listing'] }}</h1>
-    <div class="search-bar"> <input oninput="filterTable(this.value)" placeholder="{{ translations['body']['search_placeholder'] }}"> </div>
-    <div class="table-container">
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr>
-                    <th class="icon">{{ translations['body']['file'] }}</th>
-                    <th class="sortable" onclick="sortTable(1)">{{ translations['body']['name'] }}</th>
-                    <th class="sortable" onclick="sortTable(2)">{{ translations['body']['size'] }}</th>
-                    <th class="sortable" onclick="sortTable(3)">{{ translations['body']['last_modified'] }}</th>
-                </tr>
-            </thead>
-            <tbody id="fileTableBody"> {% for file in file_list %} <tr>
-                    <td class="icon"><i class="{{ file.icon }}"></i></td>
-                    <td><a href="{{ file.link }}">{{ file.name }}</a></td>
-                    <td>{{ file.size }}</td>
-                    <td>{{ file.date }}</td>
-                </tr> {% endfor %} </tbody>
-        </table>
-    </div>
-    <script>
-        function sortTable(e) {
-        const tbody = document.querySelector("table tbody");
-        const rows = Array.from(tbody.rows);
-        const isSameColumn = e === window.lastSortedColumnIndex;
-        const isAsc = isSameColumn && window.lastSortOrder === "asc";
-        const sortedRows = rows.sort((a, b) => {
-        let aText = a.cells[e].innerText.toLowerCase();
-        let bText = b.cells[e].innerText.toLowerCase();
-        if (e === 2) { // Size
-        aText = parseFloat(aText);
-        bText = parseFloat(bText);
-        } else if (e === 3) { // Last modified
-        aText = new Date(aText);
-        bText = new Date(bText);
-        }
-        if (isAsc) {
-        return aText > bText ? -1 : aText < bText ? 1 : 0;
-        } else {
-        return aText > bText ? 1 : aText < bText ? -1 : 0;
-        }
-        });
-        while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-        }
-        sortedRows.forEach(row => tbody.appendChild(row));
-        window.lastSortedColumnIndex = e;
-        window.lastSortOrder = isSameColumn && isAsc ? "desc" : "asc";
-        const parentDirRow = sortedRows.find(row => row.cells[1].innerText === "Parent Directory");
-        if (parentDirRow) {
-        tbody.prepend(parentDirRow);
-        }
-        }
+         <body>
+          <h1>{{ translations["directory_listing"] }}</h1>
+          <div class="search-bar"><input oninput="filterTable(this.value)" placeholder="{{ translations["body"]["search_placeholder"] }}"></div>
+          <table class="table table-hover table-striped">
+           <thead>
+            <tr>
+             <th class="icon">{{ translations["body"]["file"] }}</th>
+             <th class="sortable" onclick="sortTable(1)">{{ translations["body"]["name"] }}</th>
+             <th class="sortable" onclick="sortTable(2)">{{ translations["body"]["size"] }}</th>
+             <th class="sortable" onclick="sortTable(3)">{{ translations["body"]["last_modified"] }}</th>
+            </tr>
+           </thead>
+           <tbody id="fileTableBody">{% for file in file_list %}<tr>
+             <td class="icon"><i class="{{ file.icon }}"></i></td>
+             <td><a href="{{ file.link }}">{{ file.name }}</a></td>
+             <td>{{ file.size }}</td>
+             <td>{{ file.date }}</td>
+            </tr>{% endfor %}</tbody>
+          </table>
+          <script>
+           function sortTable(e){const r=document.querySelector("table tbody"),t=Array.from(r.rows),o=e===window.lastSortedColumnIndex,l=o&&"asc"===window.lastSortOrder,n=t.sort(((r,t)=>{let o=r.cells[e].innerText.toLowerCase(),n=t.cells[e].innerText.toLowerCase();return 2===e?(o=parseFloat(o),n=parseFloat(n)):3===e&&(o=new Date(o),n=new Date(n)),l?o>n?-1:o<n?1:0:o>n?1:o<n?-1:0}));for(;r.firstChild;)r.removeChild(r.firstChild);n.forEach((e=>r.appendChild(e))),window.lastSortedColumnIndex=e,window.lastSortOrder=o&&l?"desc":"asc";const s=n.find((e=>"Parent Directory"===e.cells[1].innerText));s&&r.prepend(s)}function filterTable(e){document.querySelectorAll("#fileTableBody tr").forEach((r=>{r.cells[1].innerText.toLowerCase().includes(e.toLowerCase())?r.style.display="":r.style.display="none"}))}
+          </script>
+         </body>
 
-        function filterTable(filterText) {
-        document.querySelectorAll("#fileTableBody tr").forEach(row => {
-        row.cells[1].innerText.toLowerCase().includes(filterText.toLowerCase())
-        ? row.style.display = ""
-        : row.style.display = "none";
-        });
-        }
-    </script>
-</body>
-
-</html>
-""",
+         </html>""",
         file_list=file_list,
         lang=lang,
         translations=translations,
@@ -305,18 +190,15 @@ def index(lang):
 @app.route("/<path:filename>", methods=["GET"])
 def download_file(filename):
     file_path = os.path.join(os.path.dirname(__file__), filename)
-    for part in filename.split("/"):
-        if part in set(os.getenv("ignore_files", "").split(",")):
-            return abort(403)
     if os.path.isfile(file_path):
         mime_type, _ = mimetypes.guess_type(file_path)
-        if mime_type and mime_type.split("/")[0] in {
+        if mime_type and mime_type.split("/")[0] in [
             "image",
             "audio",
             "video",
             "text",
             "application",
-        }:
+        ]:
             return send_file(file_path, mimetype=mime_type)
         return send_file(file_path, as_attachment=True)
     return abort(404)
