037977ad3483a272101d23998b65f66bc4b98cf7
RoboNamari <107060501+robonamari@users.noreply.github.com>
2025-06-21 17:23:40 +0330

    Refactor Flask app initialization and clean up unused MIME type handling in download_file function

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ from utils.translation import load_translation
 
 load_dotenv(".env")
 
-app = Flask(__name__, static_url_path="/assets", static_folder="assets")
+app = Flask(__name__, static_folder="assets")
 app.add_url_rule(
     "/favicon.ico",
     endpoint="favicon",
@@ -168,15 +168,6 @@ async def download_file(filename: str) -> Union[Response, Any]:
         if part in ignore_files:
             return abort(403)
     if os.path.isfile(file_path):
-        mime_type, _ = mimetypes.guess_type(file_path)
-        if mime_type and mime_type.split("/")[0] in {
-            "image",
-            "audio",
-            "video",
-            "text",
-            "application",
-        }:
-            return send_file(file_path, mimetype=mime_type)
         return send_file(file_path, as_attachment=True)
     return abort(404)
 
